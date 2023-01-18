5d60f89cf9840928817acb2d59c98a73a6aa9922
Khris <kaw0115@auburn.edu>
2023-01-18 11:14:21 -0600

    finish switching to active player function

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict'
 
 //elements
+const player1 = document.querySelector('.player-0');
+const player2 = document.querySelector('.player-1');
 const score0 = document.querySelector('#score-0');
 const score1 = document.querySelector('#score-1');
+const current0 = document.querySelector('#current-0');
+const current1 = document.querySelector('#current-1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn-new');
 const btnRoll = document.querySelector('.btn-roll');
@@ -13,10 +17,25 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add('hidden')
 
+let currentScore = 0;
+let activePlayer = 0
 //dice roll
 btnRoll.addEventListener('click', function() {
     const dice = Math.trunc(Math.random() * 6) + 1;
     diceEl.classList.remove('hidden');
     diceEl.src = `/images/dice-${dice}.png`;
-    console.log(dice)
+   
+
+    if(dice !== 1){
+        currentScore += dice;
+        document.getElementById(`current-${activePlayer}`).textContent = currentScore
+        
+    }else {
+        document.getElementById(`current-${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1:0;
+        player1.classList.toggle('player-active');
+        player2.classList.toggle('player-active');
+
+    }
 })
