23df7bf45c69696542d454557a96d151b493869a
Khris <kaw0115@auburn.edu>
2023-01-18 11:50:07 -0600

    add array to store player score via hold button. Declare winner

--- app.js
+++ app.js
@@ -17,10 +17,23 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add('hidden')
 
+let playing = true
 let currentScore = 0;
 let activePlayer = 0
+const scoreTracker = [0,0];
+
+const switchPlayer = function(){
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1:0;
+    player1.classList.toggle('player-active');
+    player2.classList.toggle('player-active');
+}
+
+
 //dice roll
 btnRoll.addEventListener('click', function() {
+    if(playing){
     const dice = Math.trunc(Math.random() * 6) + 1;
     diceEl.classList.remove('hidden');
     diceEl.src = `/images/dice-${dice}.png`;
@@ -31,11 +44,25 @@ btnRoll.addEventListener('click', function() {
         document.getElementById(`current-${activePlayer}`).textContent = currentScore
         
     }else {
-        document.getElementById(`current-${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1:0;
-        player1.classList.toggle('player-active');
-        player2.classList.toggle('player-active');
+        switchPlayer()
+    }
+    }
+})
 
+btnHold.addEventListener('click', function() {
+    if (playing) {
+    scoreTracker[activePlayer] += currentScore;
+    document.getElementById(`score-${activePlayer}`).textContent = scoreTracker[activePlayer]
+
+    
+
+    if (scoreTracker[activePlayer] >= 10) {
+        playing = false;
+        diceEl.classList.add('hidden')
+        document.querySelector(`.player-${activePlayer}`).classList.add('player-winner')
+        document.querySelector(`.player-${activePlayer}`).classList.remove('player-active')
+    }else {
+        switchPlayer()
+    }
     }
 })
